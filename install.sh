1951f384767bc73b8a197742a5ddca1e7f5076dd
olumayor99 <olumayoor99@gmail.com>
2023-02-28 08:12:26 +0200

    edited virtualbox install commands

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ sudo install -o root -g root -m 644 oracle_vbox_2016.gpg /etc/apt/trusted.gpg.d/
 sudo install -o root -g root -m 644 oracle_vbox.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo apt update
-sudo apt install linux-headers-$(uname -r) dkms
-sudo apt install virtualbox-7.0
+sudo apt-get install linux-headers-`uname -r` dkms build-essential
+sudo apt install -y virtualbox-7.0
 cd ~/
 VER=$(curl -s https://download.virtualbox.org/virtualbox/LATEST.TXT)
 wget https://download.virtualbox.org/virtualbox/$VER/Oracle_VM_VirtualBox_Extension_Pack-$VER.vbox-extpack
